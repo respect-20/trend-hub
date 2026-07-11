c0c8985a5b2d90e60f197c422befcbf2a01a8236
respect-20 <kuu8258@gmail.com>
2026-07-11 11:25:35 +0200

    Add custom TrendHub logo and dark mode toggle

---
+++ resources/js/Components/ApplicationLogo.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <rect x="3" y="14" width="4" height="7" rx="1" />
+        <rect x="10" y="9" width="4" height="12" rx="1" />
+        <rect x="17" y="3" width="4" height="18" rx="1" />
+    </svg>
+</template>
